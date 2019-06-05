d3a9417c18ae70de0c193f89f2f0151052522acf
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-06-05 18:09:06 +0200

    Update doomsdayclock_gui.py

--- doomsdayclock_gui.py
+++ doomsdayclock_gui.py
@@ -5,4 +5,8 @@ master = Tk()
 master.title("Doomsdayclock")
 master.geometry("600x300")
 
+title = Label(master, text = "DOOMSDAYCLOCK").place(x=0,y=0)
+text = Label(master, text = "BLA BLA BLA").place(x=0,y=20)
+
+
 glowneOkno.mainloop()
