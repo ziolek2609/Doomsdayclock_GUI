e43901a5c9aaceb255d2cb274b4891075b29ec12
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-06-06 15:31:15 +0200

    Update doomsdayclock_gui.py

--- doomsdayclock_gui.py
+++ doomsdayclock_gui.py
@@ -6,26 +6,30 @@ import datetime
 
 # funkcja licząca ile czasu pozostało do końca świata i wyświetlająca pozostały czas w odpoweidnich jednostkach
 def count():
-    # jako 'end' ustawiam date wprowadzoną przez użytkownika
-    end = datetime.datetime(int(yearbox.get()),int(monthbox.get()),int(daybox.get()),int(hourbox.get()),int(minutebox.get()),int(secondbox.get()))
-    # 'now' to dzisiejsza data
-    now = datetime.datetime.now()
-    delta = end - now
-    # obliczenia w celu uzyskania odpowiednich jednostek czasowych
-    sec = round(delta.total_seconds())
-    years = sec//(365*24*60*60)
-    days = (sec%(365*24*60*60))//(24*60*60)
-    hours = (sec%(24*60*60))//(60*60)
-    minutes = (sec%(60*60))//60
-    seconds = (sec%60)
-    # 'remained' oraz 'timer' pokazują ile czasu pozostało
-    remained = Label(master, text = "REMAINED:")
-    remained.place(x=0,y=180)
-    timer = Label(master, text =(years,"years",days,"days",hours,"hours",minutes,"minutes",seconds,"seconds"))
-    timer.place(x=0,y=200)
-    # po sekundzie funkcja wywołuje się od nowa, przez co czas spada na oczach użytkownika
-    timer.after(1000, count)
+    try:
+        # jako 'end' ustawiam date wprowadzoną przez użytkownika
+        end = datetime.datetime(int(yearbox.get()),int(monthbox.get()),int(daybox.get()),int(hourbox.get()),int(minutebox.get()),int(secondbox.get()))
+        # 'now' to dzisiejsza data
+        now = datetime.datetime.now()
+        delta = end - now
+        # obliczenia w celu uzyskania odpowiednich jednostek czasowych
+        sec = round(delta.total_seconds())
+        years = sec//(365*24*60*60)
+        days = (sec%(365*24*60*60))//(24*60*60)
+        hours = (sec%(24*60*60))//(60*60)
+        minutes = (sec%(60*60))//60
+        seconds = (sec%60)
+        # 'remained' oraz 'timer' pokazują ile czasu pozostało
+        remained = Label(master, text = "REMAINED:")
+        remained.place(x=0,y=180)
+        timer = Label(master, text =(years,"years",days,"days",hours,"hours",minutes,"minutes",seconds,"seconds"))
+        timer.place(x=0,y=200)
+        # po sekundzie funkcja wywołuje się od nowa, przez co czas spada na oczach użytkownika
+        timer.after(1000, count)
 
+    except ValueError:
+        error = Label(master, text = "incorrect data")
+        error.place(x=0,y=180)
 
 master = Tk()
 master.title("Doomsdayclock")
@@ -37,8 +41,9 @@ title = Label(master, text = "DOOMSDAYCLOCK").place(x=0,y=0)
 text = Label(master, text = "BLA BLA BLA").place(x=0,y=20)
 
 # użytkownik wprowadza datę i czas końca świata
-# TERAZ DA SIE WPISAĆ NP. 31 LUTEGO ALE WYSKOCZY WTEDY BŁĄD W KONSOLI I NIE ROZPOCZNIE SIE ODLICZANIE
+# POTRZEBA ZNALEŹĆ INNY SPOSÓB NIŻ TE SPINBOXY, BO TU MOŻEMY WPROWADZIĆ NP: 31 LUTY
 # oczywiście potrzebne opisy wszędzie
+
 yearbox = Spinbox(master, from_ = 2019, to = 3000)
 yearbox.place(x=0,y=40)
 monthbox = Spinbox(master, from_ = 1, to = 12)
@@ -64,6 +69,5 @@ secondbox.place(x=0,y=140)
 
 # przycisk rozpoczynający odliczanie
 button = Button(master, text = "count down", command = count).place(x=0,y=160)
-
 
 master.mainloop()
